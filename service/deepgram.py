92fd63089ac349ac8235495dd6a4babd94d5f917
techming <hmeng29@wisc.edu>
2023-09-05 17:30:49 -0500

    chore: added env check

--- service/deepgram.py
+++ service/deepgram.py
@@ -3,4 +3,7 @@ from dotenv import load_dotenv
 from deepgram import Deepgram
 
 load_dotenv()
-dg_client = Deepgram(os.environ.get("DEEPGRAM_API_KEY"))
+DEEPGRAM_API_KEY = os.environ.get("DEEPGRAM_API_KEY")
+if DEEPGRAM_API_KEY is None:
+    raise Exception("You need to set your Deepgram API Key as environment variable before running the project.")
+dg_client = Deepgram(DEEPGRAM_API_KEY)
